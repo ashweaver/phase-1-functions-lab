a00170dc584c28f6125f7c8a64a350c02b303beb
ashweaver <97565194+ashweaver@users.noreply.github.com>
2022-03-01 10:06:17 -0600

    commit tree ad87f633affc73e739010a9ae68225b252873196

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+const hqLocation = 42
+function distanceFromHqInBlocks(someValue){
+
+if (someValue > 42){
+    return (someValue - hqLocation);
+
+}else {
+    return (hqLocation -someValue);
+}
+}
+function distanceFromHqInFeet (someValue){
+    distanceFromHqInBlocks (someValue);
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+function distanceTravelledInFeet(hqLocation, someValue){
+    const destFt = (someValue * 264)
+    const hqFt = (hqLocation * 264)
+    if (destFt > hqFt){
+        return (destFt - hqFt)
+    }else {
+        return (hqFt - destFt)
+    } 
+}
+
+
+function calculatesFarePrice(hqLocation, someValue) {
+  const distanceInFeet = distanceTravelledInFeet(hqLocation, someValue);
+  if (distanceInFeet <= 400) {
+    return 0;
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return 0.02 * (distanceInFeet - 400);  
+  } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
